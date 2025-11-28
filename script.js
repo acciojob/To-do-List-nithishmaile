c833c011c73436a36354c605c3ba351da634c069
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-11-28 21:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
-
+const addTodo=document.getElementById("addTodoBtn");
+const input=document.getElementById("newTodoInput");
+const ol=document.getElementById("todoList");
+addTodo.addEventListener("click",()=>{
+	if(input.value){
+		const li=document.createElement("li");
+		li.innerText=input.value
+		ol.appendChild(li);
+	}
+	input.value=""
+})
